083fc530ac1e10d44b100b862cf97847400ba39b
Carlos Rivera <riverautx@gmail.com>
2014-01-16 19:30:29 -0600

    added new icon images

--- code/js/background/app.js
+++ code/js/background/app.js
@@ -1,23 +1,11 @@
 if (typeof require !== 'undefined') {
     var ExtensionApi = require("./extension_api/firefox.js").ExtensionApi;
     var Constants = require("./constants.js").Constants;
-    var _ = require("./lib/underscore");
 }
 
 var App = (function() {
-    function _sendMessageToCurrentTab(msg, data) {
-        data = data || {};
-        ExtensionApi.getCurrentTab(function(tab) {
-            ExtensionApi.sendMessage(tab, msg, data);
-        });
-    }
-
     return {
-        init : function(options) {
-            ExtensionApi.getCurrentTab(function(tab) {
-                ExtensionApi.enableIcon(tab);
-            });
-
+        init : function() {
             // Listen for message from content scripts
             ExtensionApi.onMessage("do-something-bg", function(request, tab) {
                 alert("Doing something in the Background");
